f8a08ab7781f48526e9925f04d5b9cef2aa5404b
Raphoudre <raphael.ivol@hotmail.fr>
2020-11-20 16:34:10 +0100

    ajout de plusieurs sections et modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@
 
 
 //definir les variables
+let moreProd = 0;
+
 let nbFifa = 0;
 let nbFmanager = 0;
 let nbCyberpunk = 0;
@@ -21,10 +23,12 @@ let goCatalogue = document.getElementById("goCatalogue");
 let goPanier = document.getElementById("goPanier");
 let goFastCatalogue = document.getElementById("fastCatalogue");
 //apparitions
-let ourTeamB = document.getElementById("ourTeamButton");
 
 //dans panier
 let fifa = document.getElementById("addCartFifa");
+let fifaPrix = 90;
+let fmanager = document.getElementById("addCartFManager")
+let fmanagerPrix = 45;
 
 let cartDesc = document.getElementById("cartDesc");
 
@@ -36,25 +40,57 @@ let catalogue = document.getElementById("catalogue");
 let panier = document.getElementById("panier");
 let infoUs = document.getElementById("infoUs")
 
-ourTeamB.onclick = function(){
-    infoUs.classList.toggle("invisible");
-    infoUs.classList.toggle("visible");
+let closeCar = document.getElementById("closeCar");
+closeCar.onclick = function(){
+    document.getElementById("carouselExampleControls").classList.toggle("invisible")
+    closeCar.classList.toggle("invisible")
 }
 
-//panier
-fifa.onclick = function(){
-    if (nbFifa==0){
-        nbFifa = 1;
-        cartDesc.innerHTML=`<li><img  src="assets/img/cameraroll/fifa-21-ps4-france-cover.jpg" height="50rem"alt=""> Fifa | ${nbFifa} exemplaire</li>`;
-    } else {
-        nbFifa += 1;
-        cartDesc.innerHTML=`<li><img src="assets/img/cameraroll/fifa-21-ps4-france-cover.jpg" height="50rem"alt=""> Fifa | ${nbFifa} exemplaire</li>`;
-    }
+const allCross = document.querySelectorAll('.visible-pannel img');
+console.log(allCross);
+
+
+allCross.forEach(element => {
+
+    element.addEventListener('click', function(){
+
+        const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
+
+        const currentChoice = this.parentNode.parentNode.childNodes[3];
+
+        // console.log(this.src);
+        if(this.src.includes('angle-down-solid')){
+            this.src = 'assets/img/angle-up-solid.svg';
+            gsap.to(currentChoice, {duration: 0.2, height: height + 40, opacity: 1, padding: '20px 15px'})
+        } else if (this.src.includes('angle-up-solid')){
+            this.src = 'assets/img/angle-up-solid.svg';
+            gsap.to(currentChoice, {duration: 0.2, height: 0, opacity: 0, padding: '0px 15px'})
+        }
+    })
+})
+
+const parallax = document.getElementById('parallax');
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    parallax.style.backgroundPositionY = -window.scrollY / 3 + "px";
+});
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-let closeCar = document.getElementById("closeCar")
-
-closeCar.onclick = function (){
-    document.getElementById("carouselExampleControls").classList.toggle("invisible");
-    closeCar.classList.toggle("invisible");
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
